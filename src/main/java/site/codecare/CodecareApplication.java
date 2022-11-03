ba797cd4e9705d7de6e7b3eec37a57d3aec70c39
KCH <doolysmile1@naver.com>
2022-11-03 22:19:04 +0900

    initial commit

---
+++ src/main/java/site/codecare/CodecareApplication.java
@@ -0,0 +1,13 @@
+package site.codecare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodecareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/codecare/CodecareApplicationTests.java
@@ -0,0 +1,13 @@
+package site.codecare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodecareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
