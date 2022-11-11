0c4f0f8a7d170a8f4ae354cd909639dc57efed9a
brightGarden02 <rreone2017@gmail.com>
2022-11-11 18:28:22 +0900

    feat: 환경세팅

--- src/main/java/site/codecare/CodecareApplication.java
+++ src/main/java/site/codecare/CodecareApplication.java
@@ -1,13 +1,26 @@
 package site.codecare;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableBatchProcessing
+@EnableJpaAuditing
 public class CodecareApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodecareApplication.class, args);
 	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
--- src/test/java/site/codecare/CodecareApplicationTests.java
+++ src/test/java/site/codecare/CodecareApplicationTests.java
@@ -2,8 +2,12 @@ package site.codecare;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
+@ActiveProfiles("test")
 class CodecareApplicationTests {
 
 	@Test
