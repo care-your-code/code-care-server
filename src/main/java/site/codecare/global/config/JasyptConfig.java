3eebf9ba9fd6c5f25974daa0d6bebafb3f02bdcc
KCH <doolysmile1@naver.com>
2022-11-22 21:35:29 +0900

    chore : 파일에서 키 불러오도록 수정

--- src/main/java/site/codecare/global/config/JasyptConfig.java
+++ src/main/java/site/codecare/global/config/JasyptConfig.java
@@ -6,18 +6,22 @@ import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 @Configuration
 public class JasyptConfig {
 
-    @Value("${jasypt.encryptor.password}")
-    private String encryptKey;
 
     @Bean("jasyptStringEncryptor")
     public StringEncryptor stringEncryptor(){
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword(encryptKey);
+        config.setPassword(getJasyptEncryptorPassword());
         config.setPoolSize("1");
         config.setAlgorithm("PBEWithMD5AndDES");
         config.setStringOutputType("base64");
@@ -25,5 +29,15 @@ public class JasyptConfig {
         config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
         encryptor.setConfig(config);
         return encryptor;
+    }
+    private String getJasyptEncryptorPassword() {
+        try {
+            ClassPathResource resource = new ClassPathResource("jasypt-encryptor-password.txt");
+            return Files.readAllLines(Paths.get(resource.getURI())).stream()
+                    .collect(Collectors.joining(""));
+        } catch (IOException e) {
+            System.out.println(new ClassPathResource("herehere"));
+            throw new RuntimeException("Not found Jasypt password file.");
+        }
     }
 }
