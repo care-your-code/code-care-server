f2b37db02764aa06b0b97d290e521129aa17d411
KCH <doolysmile1@naver.com>
2022-11-22 19:16:29 +0900

    test : jasypt 테스트 추가

---
+++ src/test/java/site/codecare/global/config/JasyptConfigTest.java
@@ -0,0 +1,24 @@
+package site.codecare.global.config;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JasyptConfigTest extends JasyptConfig{
+
+    @Test
+    public void jasypt_encrypt_decrypt_test() {
+        String plainText = "code_care1234!";
+
+        StandardPBEStringEncryptor jasypt = new StandardPBEStringEncryptor();
+        jasypt.setPassword("password");
+
+        String encryptedText = jasypt.encrypt(plainText);
+        String decryptedText = jasypt.decrypt(encryptedText);
+
+        System.out.println("here" + encryptedText);
+
+        assertThat(plainText).isEqualTo(decryptedText);
+    }
+}
