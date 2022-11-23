0ea5e3547bbdbaae55bda7c7afa3bac41ed26d57
KCH <doolysmile1@naver.com>
2022-11-23 19:19:26 +0900

    chore : 암호화 방식 수정

--- db/init.sql
+++ db/init.sql
@@ -3,10 +3,8 @@ DROP DATABASE IF EXISTS code_care_test;
 CREATE DATABASE code_care_test;
 USE code_care_test;
 
-# 개발 DB 생성
-DROP DATABASE IF EXISTS code_care_dev;
-CREATE DATABASE code_care_dev;
-USE code_care_dev;
+
+
 
 # 로컬 DB 생성
 DROP DATABASE IF EXISTS code_care_local;
--- src/main/java/site/codecare/global/config/JasyptConfig.java
+++ src/main/java/site/codecare/global/config/JasyptConfig.java
@@ -3,6 +3,7 @@ package site.codecare.global.config;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
@@ -15,12 +16,13 @@ import java.util.stream.Collectors;
 @Configuration
 public class JasyptConfig {
 
-
+    @Value("${jasypt.encryptor.password}")
+    private String PASSWORD;
     @Bean("jasyptStringEncryptor")
     public StringEncryptor stringEncryptor(){
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword(getJasyptEncryptorPassword());
+        config.setPassword(PASSWORD);
         config.setPoolSize("1");
         config.setAlgorithm("PBEWithMD5AndDES");
         config.setStringOutputType("base64");
@@ -29,14 +31,14 @@ public class JasyptConfig {
         encryptor.setConfig(config);
         return encryptor;
     }
-    private String getJasyptEncryptorPassword() {
-        try {
-            ClassPathResource resource = new ClassPathResource("jasypt-encryptor-password.txt");
-            return Files.readAllLines(Paths.get(resource.getURI())).stream()
-                    .collect(Collectors.joining(""));
-        } catch (IOException e) {
-            System.out.println(new ClassPathResource("herehere"));
-            throw new RuntimeException("Not found Jasypt password file.");
-        }
-    }
+//    private String getJasyptEncryptorPassword() {
+//        try {
+//            ClassPathResource resource = new ClassPathResource("jasypt-encryptor-password.txt");
+//            return Files.readAllLines(Paths.get(resource.getURI())).stream()
+//                    .collect(Collectors.joining(""));
+//        } catch (IOException e) {
+//            System.out.println(new ClassPathResource("herehere"));
+//            throw new RuntimeException("Not found Jasypt password file.");
+//        }
+//    }
 }
