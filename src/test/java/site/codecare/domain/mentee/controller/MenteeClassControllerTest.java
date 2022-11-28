e972942ac1aeb2eddfeedf58a9a9ae9f976f6865
brightGarden02 <rreone2017@gmail.com>
2022-11-28 21:53:41 +0900

    refactor: spring docs 적용

---
+++ src/test/java/site/codecare/domain/mentee/controller/MenteeClassControllerTest.java
@@ -0,0 +1,14 @@
+package site.codecare.domain.mentee.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(value = MenteeClassController.class)
+class MenteeClassControllerTest {
+
+}
--- src/test/java/site/codecare/domain/mentee/service/MenteeClassServiceTest.java
+++ src/test/java/site/codecare/domain/mentee/service/MenteeClassServiceTest.java
@@ -3,6 +3,7 @@ package site.codecare.domain.mentee.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @Transactional
 @ActiveProfiles("test")
+//@MockBean
 class MenteeClassServiceTest {
 
     @Autowired
@@ -77,12 +79,11 @@ class MenteeClassServiceTest {
     /**
      * 멘티 개인정보 수정
      */
-    @Test
-    void update_mentee_info() throws Exception {
-
-
-
-    }
+//    @Test
+//    void update_mentee_info() throws Exception {
+//
+//
+//    }
 
 
 
