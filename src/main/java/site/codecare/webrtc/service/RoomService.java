c2a028a46099751dc2d7122305bbb0231de4a120
ChangMinSuh <timssuh@naver.com>
2022-12-01 07:43:49 +0900

    test : room client 추가 및 AopContext로 SelfInvocation 해결

--- src/main/java/site/codecare/webrtc/service/RoomService.java
+++ src/main/java/site/codecare/webrtc/service/RoomService.java
@@ -2,22 +2,24 @@ package site.codecare.webrtc.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.socket.WebSocketSession;
 import site.codecare.webrtc.entity.Room;
 
+import javax.annotation.Resource;
 import java.util.Optional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class RoomService {
-    private static final Room EMPTY_DATA = new Room("");
 
     @Cacheable(cacheNames = "webrtcRoom", key = "#name")
     public Optional<Room> findRoomByName(String name){
-//        log.info("해당 room name은 cache에 존재하지 않습니다.");
+          log.info("해당 room name은 cache에 존재하지 않습니다.");
 //        throw new RuntimeException("no room");
         return Optional.empty();
     }
@@ -25,11 +27,24 @@ public class RoomService {
     @CachePut(cacheNames = "webrtcRoom", key = "#name")
     public Room saveRoom(String name) {
         // 중복된 이름 방 체크
-        Optional<Room> room = findRoomByName(name);
-        if(room.isPresent()) throw new RuntimeException("이미 존재하는 방 이름입니다.");
+        Optional<Room> findRoom = ((RoomService) AopContext.currentProxy()).findRoomByName(name);
+        if(findRoom.isPresent()) throw new RuntimeException("이미 존재하는 방 이름입니다.");
 
         // 방 생성
         Room saveRoom = new Room(name);
         return saveRoom;
+    }
+
+    @CachePut(cacheNames = "webrtcRoom", key = "#name")
+    public Room addClient(String name, String email, WebSocketSession session) {
+        log.info(name);
+        // 이름에 맞는 방 찾음
+        Room findRoom = ((RoomService) AopContext.currentProxy()).findRoomByName(name)
+                .orElseThrow(() -> new RuntimeException("방이 존재하지 않습니다."));
+
+        // 방에 client 정보 추가
+        findRoom.getClients().put(email, session);
+
+        return findRoom;
     }
 }
--- src/test/java/site/codecare/webrtc/RoomServiceTest.java
+++ src/test/java/site/codecare/webrtc/RoomServiceTest.java
@@ -1,27 +1,44 @@
 package site.codecare.webrtc;
 
+import org.junit.Before;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.socket.WebSocketSession;
 import site.codecare.webrtc.entity.Room;
 import site.codecare.webrtc.service.RoomService;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 @SpringBootTest
 @Transactional
 @ActiveProfiles("test")
+@EnableAspectJAutoProxy(exposeProxy=true)
 public class RoomServiceTest {
 
-    @Autowired
-    RoomService roomService;
+    @Autowired private RoomService roomService;
+
+    private static String email;
+    private static String roomName;
+    private static WebSocketSession session;
+
+    @BeforeAll
+    public static void setup() {
+        email = "test1@test.com";
+        session = mock(WebSocketSession.class);
+    }
 
     @Test
     public void 과외방_생성_및_확인(){
         // Arrange
-        String roomName = "교육 방1";
+        roomName = "교육 방1";
         Room room = new Room(roomName);
 
         // Act
@@ -37,5 +54,45 @@ public class RoomServiceTest {
                 .isNotNull()
                 .usingRecursiveComparison()
                 .isEqualTo(room);
+    }
+
+    @Test
+    public void 과외방_중복_생성(){
+        Assertions.assertThrows(
+                // Assert
+                RuntimeException.class,
+                () -> {
+                    // Arrange
+                    roomName = "교육 방2";
+                    Room room = new Room(roomName);
+
+                    // Act
+                    roomService.saveRoom(roomName);
+
+                    roomService.saveRoom(roomName);
+                });
+    }
+
+
+    @Test
+    public void 과외방에_인원_추가_및_확인(){
+        // Arrange
+        roomName = "교육 방3";
+        roomService.saveRoom(roomName);
+
+        // Act
+        // select room
+        roomService.addClient(roomName, email, session);
+
+        // select room
+        Room findRoom = roomService.findRoomByName(roomName)
+                .orElseThrow(() -> new RuntimeException("괴외방 추가 및 확인 error"));
+
+
+        //Assert
+        assertThat(findRoom)
+                .isNotNull();
+        assertThat(findRoom.getClients().get(email))
+                .isSameAs(session);
     }
 }
