bd5189bb68269ba86103f01e9d20494206a80dfd
Shin-sangwon <dnjsml10@gmail.com>
2022-12-08 17:34:37 +0900

    CC-3 test : Community create test 생성

--- src/test/java/site/codecare/domain/community/service/CommunityServiceTests.java
+++ src/test/java/site/codecare/domain/community/service/CommunityServiceTests.java
@@ -1,11 +1,35 @@
 package site.codecare.domain.community.service;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Transactional
 @SpringBootTest
 @ActiveProfiles("test")
 public class CommunityServiceTests {
+
+    @Test
+    @DisplayName("게시물_저장된다")
+    void t1() throws Exception {
+
+        //Arrange
+        Post post = Post.builder()
+                .title("title")
+                .content("content")
+                .email("code-care@naver.com")
+                .build();
+        //Act
+
+        postRepository.save(post);
+
+        //Assert
+
+        assertThat(postRepository.findById(1L)).isNotNull();
+
+    }
 }
