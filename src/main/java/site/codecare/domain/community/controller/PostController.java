6241fd4cd6773f76d1399c3b0c5522eb0ee41172
Shin-sangwon <dnjsml10@gmail.com>
2022-12-08 19:47:35 +0900

    CC-3 feat : Post findById api 구현

--- src/main/java/site/codecare/domain/community/controller/PostController.java
+++ src/main/java/site/codecare/domain/community/controller/PostController.java
@@ -1,8 +1,13 @@
 package site.codecare.domain.community.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import site.codecare.domain.community.dto.PostResponseDto;
 import site.codecare.domain.community.service.PostService;
 
 @RequiredArgsConstructor
@@ -11,6 +16,16 @@ import site.codecare.domain.community.service.PostService;
 public class PostController {
 
     private final PostService postService;
+
+    @GetMapping("/post/{id}")
+    public ResponseEntity<PostResponseDto> findById(@PathVariable Long id) {
+
+        PostResponseDto post = postService.findById(id);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(post);
+    }
 
 
 }
